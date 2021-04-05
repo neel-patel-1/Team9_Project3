0faecc445f291c3e3385fd49c2e6286f798432ed
Neel Patel <neelpatelbiz@gmail.com>
2021-04-04 23:55:40 -0500

    draw w a square

---
+++ js/test.js
@@ -0,0 +1,70 @@
+document.querySelector('#hordeGame').addEventListener('click', () => {
+    //clear child elements of game div
+    document.querySelector('#game').textContent = ' ';
+    document.querySelector('#instructions').textContent = ' ';
+    //append instructions
+    let instr = document.createElement('p');
+    instr.textContent = 'Move the Square!';
+    document.querySelector('#instructions').appendChild(instr);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    document.getElementById("game").appendChild( canvas );
+    const ctx = canvas.getContext('2d');
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+
+    const rect = {
+        l : 20,
+        w : 20,
+        x : canvas.width/2,
+        y : canvas.width/2,
+        moveV : function(dir){
+            if(dir === 0){
+                this.y-=(this.l/2);
+            }
+            else{
+                this.y+=(this.l/2);
+            }
+        },
+        moveH : function(dir){
+            if(dir === 0){
+                this.x-=(this.w/2);
+            }
+            else{
+                this.x+=(this.w/2);
+            }
+        },
+        draw: function(){
+            ctx.fillStyle = 'Red';
+            ctx.fillRect(this.x,this.y,this.w,this.l);
+        }
+    }
+
+    window.addEventListener("keydown", function(event) {
+        console.log(`KeyboardEvent: key='${event.key}' | code='${event.code}'`);
+        if(event.key === 'd' || event.key === 'ArrowRight'){
+            rect.moveH(1);
+        }
+        if(event.key === 'a' || event.key === 'ArrowLeft'){
+            rect.moveH(0);
+        }
+        if(event.key === 's' || event.key === 'ArrowDown'){
+            rect.moveV(1);
+        }
+        if(event.key === 'w' || event.key === 'ArrowUp'){
+            rect.moveV(0);
+        }
+        rect.draw();
+        
+    });
+    
+    window.addEventListener('resize', onWindowResize, false);
+    function onWindowResize(){
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        ctx.fillRect(0,0,canvas.width, canvas.height);
+    }
+    rect.draw();
+})
+
