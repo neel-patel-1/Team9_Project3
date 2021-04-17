f89fc0f69eb3ab937d8d42f21afcd2ff8867da48
Neel Patel <neelpatelbiz@gmail.com>
2021-04-17 00:47:14 -0500

    drop-down menu for selecting

--- js/menu-interact.js
+++ js/menu-interact.js
@@ -5,6 +5,38 @@ const game3dFuncs = [tictactoeinit, matchingInit, ballInit];
 const game2dArray = ["#snake", "#swarm"];
 const game2dFuncs = [snakeInit, swarmInit];
 
+// for(let i=0; i<game3dArray.length; i++){
+//     document.querySelector(game3dArray[i]).addEventListener('click', () =>{
+//         document.querySelector('#home').hidden = true;
+//         document.querySelector('#artdiv').hidden = true;
+//         let gameEls= document.querySelector('#gamediv').children;
+//         for (let i=0; i<gameEls.length; i++){
+//             gameEls[i].innerHTML = '';
+//         }
+//         game3dFuncs[i];
+//         document.querySelector('#widget-menu').hidden = true;
+//         document.querySelector('#gamediv').hidden = false;
+//     })
+// }
+// function testFunc(){
+//     game2dFuncs[0];
+//     document.querySelector('#widget-menu').hidden = true;
+//     document.querySelector('#gamediv').hidden = false;
+// }
+for(let i=0; i<game3dArray.length; i++){
+    document.querySelector(game3dArray[i]).addEventListener('click', () => {
+        game3dFuncs[i]();
+        document.querySelector('#widget-menu').style.display = "none";
+        document.querySelector('#gamediv').hidden = false;
+    })
+}
+for(let i=0; i<game2dArray.length; i++){
+    document.querySelector(game2dArray[i]).addEventListener('click', () => {
+        game2dFuncs[i]();
+        document.querySelector('#widget-menu').style.display = "none";
+        document.querySelector('#gamediv').hidden = false;
+    })
+}
 document.querySelector('#select_home').addEventListener('click', () => {
     let gameEls= document.querySelector('#gamediv').children;
     for (let i=0; i<gameEls.length; i++){
@@ -25,12 +57,10 @@ document.querySelector('#select_3d').addEventListener('click', () => {
     for(let i=0; i<game3dArray.length; i++){
         let game = document.createElement('button');
         game.innerText = game3dArray[i].slice(1);
-        document.querySelector('.widget-menu').appendChild(game)
+        document.querySelector('#widget-menu').appendChild(game)
             .addEventListener('click', game3dFuncs[i]);
     }
-
-    //
-    //below useless
+    document.querySelector('#widget-menu').style.display = "flex";
 })
 document.querySelector('#select_2d').addEventListener('click', () => {
     document.querySelector('#home').hidden = true;
@@ -40,16 +70,13 @@ document.querySelector('#select_2d').addEventListener('click', () => {
         gameEls[i].innerHTML = '';
     }
     document.querySelector('#gamediv').hidden=false;
-    //
     for(let i=0; i<game2dArray.length; i++){
         let game = document.createElement('button');
-        //game.className='widget-menu';
         game.innerText = game2dArray[i].slice(1);
-        document.querySelector('.widget-menu').appendChild(game)
+        document.querySelector('#widget-menu').appendChild(game)
                     .addEventListener('click', game2dFuncs[i]);
     }
-    //
-    
+    document.querySelector('#widget-menu').style.display = "flex";
 })
 document.querySelector('#art').addEventListener('click', () => {
     let gameEls= document.querySelector('#gamediv').children;
